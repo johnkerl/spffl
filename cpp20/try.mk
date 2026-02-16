8faa3de9f0528ad389ebb29ac4f69ec0bd5f7187
John Kerl <kerl.john.r@gmail.com>
2026-02-16 11:33:24 -0500

    try.mk

--- cpp20/try.mk
+++ cpp20/try.mk
@@ -1,10 +1,13 @@
+# Out-of-source build in build/. Remove in-source CMake state so FetchContent
+# runs in build/ and gets a complete Catch2 (avoids broken cpp20/_deps).
 build:
+	rm -rf CMakeCache.txt _deps
 	mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release && cmake --build .
 
 test:
 	cd build && ctest --output-on-failure
 
 clean:
-	rm -rf build _deps
+	rm -rf build _deps CMakeCache.txt CMakeFiles Makefile cmake_install.cmake CTestTestfile.cmake Testing generated-includes
 
 .PHONY: build test clean
