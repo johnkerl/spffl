54b677972ef91b8866d8eb49f464a3871f5075b9
John Kerl <kerl.john.r@gmail.com>
2026-02-17 13:23:51 -0500

    clang vs g++

--- spffl/polynomials/fp_poly_t.h
+++ spffl/polynomials/fp_poly_t.h
@@ -2,16 +2,14 @@
 // fp_poly_t: Fp[x] as polynomial_of<intmod_t> (Phase 1.3 migration).
 // Include this for fp_poly_t and legacy I/O helpers.
 // ================================================================
+// Note: gcd(fp_poly_t, fp_poly_t) is provided by polynomial_of<> friend in
+// polynomial_of.hpp. Do not add a separate overload here or GCC will see
+// ambiguity with that friend when resolving gcd in vector_over::vgcd().
 
 #ifndef FPPOLY_T_H
 #define FPPOLY_T_H
 
 #include "spffl/polynomials/aliases.hpp"
 #include "spffl/polynomials/fp_poly_io.hpp"
-
-// Legacy: free function gcd (also available as member and friend in polynomial_of).
-namespace spffl::polynomials {
-inline fp_poly_t gcd(const fp_poly_t& a, const fp_poly_t& b) { return a.gcd(b); }
-}
 
 #endif  // FPPOLY_T_H
